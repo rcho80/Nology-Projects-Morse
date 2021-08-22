d0eeba3b92cf7ed845ad75d99f1aa08657d870b9
_RC_ <55364865+rcho80@users.noreply.github.com>
2021-08-22 16:26:53 +1000

    latest version

--- letters.js
+++ letters.js
@@ -1,187 +1,136 @@
-export const morseLetters = [{
-    key: "a",
-    cap: "A",
-    code: ".- "
-  },
-  {
-    key: "b",
-    cap: "B",
-    code: "-... "
-  },
-  {
-    key: "c",
-    cap: "C",
-    code: "-.-. "
-  },
-  {
-    key: "d",
-    cap: "D",
-    code: "-.. "
-  },
-  {
-    key: "e",
-    cap: "E",
-    code: ". "
-  },
-  {
-    key: "f",
-    cap: "F",
-    code: "..-. "
-  },
-  {
-    key: "g",
-    cap: "G",
-    code: "--. "
-  },
-  {
-    key: "h",
-    cap: "H",
-    code: ".... "
-  },
-  {
-    key: "i",
-    cap: "I",
-    code: ".. "
-  },
-  {
-    key: "j",
-    cap: "J",
-    code: ".--- "
-  },
-  {
-    key: "k",
-    cap: "K",
-    code: "-.- "
-  },
-  {
-    key: "l",
-    cap: "L",
-    code: ".-.. "
-  },
-  {
-    key: "m",
-    cap: "M",
-    code: "-- "
-  },
-  {
-    key: "n",
-    cap: "N",
-    code: "-. "
-  },
-  {
-    key: "o",
-    cap: "O",
-    code: "--- "
-  },
-  {
-    key: "p",
-    cap: "P",
-    code: ".--. "
-  },
-  {
-    key: "q",
-    cap: "Q",
-    code: "--.- "
-  },
-  {
-    key: "r",
-    cap: "R",
-    code: ".-. "
-  },
-  {
-    key: "s",
-    cap: "S",
-    code: "... "
-  },
-  {
-    key: "t",
-    cap: "T",
-    code: "- "
-  },
-  {
-    key: "u",
-    cap: "U",
-    code: "..- "
-  },
-  {
-    key: "v",
-    cap: "V",
-    code: "...- "
-  },
-  {
-    key: "w",
-    cap: "W",
-    code: ".-- "
-  },
-  {
-    key: "x",
-    cap: "X",
-    code: "-..- "
-  },
-  {
-    key: "y",
-    cap: "Y",
-    code: "-.-- "
-  },
-  {
-    key: "z",
-    cap: "Z",
-    code: "--.. "
-  },
-  {
-    key: " ",
-    code: " / "
-  },
-  {
-    key: ".",
-    code: ". _ . _ . _ "
-  },
-  {
-    key: ",",
-    code: "_ _ . . _ _ "
-  },
-  {
-    key: "'",
-    code: ". _ _ _ _ . "
-  },
-  {
-    key: "1",
-    code: ".---- "
-  },
-  {
-    key: "2",
-    code: "..--- "
-  },
-  {
-    key: "3",
-    code: "...-- "
-  },
-  {
-    key: "4",
-    code: "....- "
-  },
-  {
-    key: "5",
-    code: "..... "
-  },
-  {
-    key: "6",
-    code: "-.... "
-  },
-  {
-    key: "7",
-    code: "--... "
-  },
-  {
-    key: "8",
-    code: "---.. "
-  },
-  {
-    key: "9",
-    code: "----. "
-  },
-  {
-    key: "0",
-    code: "----- "
-  },
+export const morseLetters = [
+    {
+        key: "a",
+        cap: "A",
+        code: ".- "
+    },
+    {
+        key: "b",
+        cap: "B",
+        code: "-... "
+    },
+    {
+        key: "c",
+        cap: "C",
+        code: "-.-. "
+    },
+    {
+        key: "d",
+        cap: "D",
+        code: "-.. "
+    },
+    {
+        key: "e",
+        cap: "E",
+        code: ". "
+    },
+    {
+        key: "f",
+        cap: "F",
+        code: "..-. "
+    },
+    {
+        key: "g",
+        cap: "G",
+        code: "--. "
+    },
+    {
+        key: "h",
+        cap: "H",
+        code: ".... "
+    },
+    {
+        key: "i",
+        cap: "I",
+        code: ".. "
+    },
+    {
+        key: "j",
+        cap: "J",
+        code: ".--- "
+    },
+    {
+        key: "k",
+        cap: "K",
+        code: "-.- "
+    },
+    {
+        key: "l",
+        cap: "L",
+        code: ".-.. "
+    },
+    {
+        key: "m",
+        cap: "M",
+        code: "-- "
+    },
+    {
+        key: "n",
+        cap: "N",
+        code: "-. "
+    },
+    {
+        key: "o",
+        cap: "O",
+        code: "--- "
+    },
+    {
+        key: "p",
+        cap: "P",
+        code: ".--. "
+    },
+    {
+        key: "q",
+        cap: "Q",
+        code: "--.- "
+    },
+    {
+        key: "r",
+        cap: "R",
+        code: ".-. "
+    },
+    {
+        key: "s",
+        cap: "S",
+        code: "... "
+    },
+    {
+        key: "t",
+        cap: "T",
+        code: "- "
+    },
+    {
+        key: "u",
+        cap: "U",
+        code: "..- "
+    },
+    {
+        key: "v",
+        cap: "V",
+        code: "...- "
+    },
+    {
+        key: "w",
+        cap: "W",
+        code: ".-- "
+    },
+    {
+        key: "x",
+        cap: "X",
+        code: "-..- "
+    },
+    {
+        key: "y",
+        cap: "Y",
+        code: "-.-- "
+    },
+    {
+        key: "z",
+        cap: "Z",
+        code: "--.. "
+    },
+    {
+        key: " ",
+        code: "/ "
+    }
 ];
--- script.js
+++ script.js
@@ -2,47 +2,69 @@ import { morseLetters } from "./letters.js";
 
 const textEng = document.getElementById("text__eng");
 const textMorse = document.getElementById("text__morse");
+const submitMorse = document.getElementById("submit__morse");
 const clear = document.getElementById("btn__clear");
-const morseTxt = document.getElementById("morseTxt");
 
 // handle English to morse
 const handleLetters = (keys) => {
-  morseLetters.map((key) => {
-    if (keys.key === key.key || keys.key === key.cap) {
-      morseTxt.value += key.code;
-    }
-  });
+    morseLetters.map((key) => {
+        if (keys.key === key.key || keys.key === key.cap) {
+            textMorse.value += key.code;
+        }
+    });
 
-  if (keys.key === "Backspace") {
-    const arrStr = morseTxt.value.trim().split(" ");
-    const arrPop = arrStr.pop();
-    morseTxt.value = arrStr.join(" ");
-  }
+    if (
+        keys.key === "Backspace" ||
+        keys.key === "Delete" ||
+        keys.key === "Insert"
+    ) {
+        keys.preventDefault();
+    }
 };
 textEng.addEventListener("keydown", handleLetters);
 
 //handle Morse to English
+let anotherGroup = [];
+
 const handleMorse = (keys) => {
-  const container = [];
-  const splitted = morseTxt.value.split(" ");
-  for (let i = 0; i < splitted.length; i++) {
-    morseLetters.filter((letter) => {
-      letter.code = letter.code.trim();
-      if (letter.code === splitted[i]) {
-        container.push(letter.key);
-      }
-    });
-  }
-  textEng.value += container.join("");
+    if (textEng.value !== "") {
+        return false;
+    }
+
+    const splitted = textMorse.value.split(" ");
+
+    for (let i = 0; i < splitted.length; i++) {
+        anotherGroup.push(`${splitted[i]} `);
+    }
+
+    for (let j = 0; j < anotherGroup.length; j++) {
+        morseLetters.filter((letter) => {
+            if (letter.code === anotherGroup[j]) {
+                textEng.value += letter.key;
+            }
+        });
+    }
 };
-text__morse.addEventListener("click", handleMorse);
+submitMorse.addEventListener("click", handleMorse);
+
+//handle preventdefault of delete in morseBox
+const handleMorseBox = (keys) => {
+    if (
+        keys.key === "Backspace" ||
+        keys.key === "Delete" ||
+        keys.key === "Insert"
+    ) {
+        keys.preventDefault();
+    }
+};
+
+textMorse.addEventListener("keydown", handleMorseBox);
 
 // handle Clear button delete All
 const handleClear = () => {
-  morseTxt.innerText = "";
-  morseTxt.value = "";
-  textEng.innerText = "";
-  textEng.value = "";
+    textMorse.value = "";
+    textEng.value = "";
+    anotherGroup = [];
 };
 
 clear.addEventListener("click", handleClear);
